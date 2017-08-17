a7ded811f252a098b61415b9e1536acf52fb9ab8
bernat <bernat@traveltainment.de>
2017-08-17 18:04:31 +0200

    kongos drink

--- core/src/com/gamefactoryx/cheers/tool/kongosdrink/CardTextParser.java
+++ core/src/com/gamefactoryx/cheers/tool/kongosdrink/CardTextParser.java
@@ -4,65 +4,9 @@ import com.gamefactoryx.cheers.model.kongosdrink.KongosDrinkMainModel;
 import com.gamefactoryx.cheers.model.kongosdrink.Player;
 
 public class CardTextParser {
-    public enum SEX_OF_PLAYER {UNI, HOMO, HETERO}
-    public enum PARAM_TYPE {POINTS, SIPS, TIMER}
-
-    /*public static void main(String[] args) {
-        String line = "--Player-, trinkt 2 mal mit dir!;1;2;0";
-        line = replacePlayerNames(line);
-        System.out.println(replace(line));
-        System.out.println(getValue(PARAM_TYPE.POINTS,line));
-        System.out.println(getValue(PARAM_TYPE.SIPS,line));
-        System.out.println(getValue(PARAM_TYPE.TIMER,line));
-    }*/
-
-    /*public static SEX_OF_PLAYER getSexOfPlayer(String line) {
-        if (line.contains("-oPlayer-")) return SEX_OF_PLAYER.UNI;
-        else if (line.contains("--Player-")) return SEX_OF_PLAYER.HOMO;
-        else if (line.contains("-+Player-")) return SEX_OF_PLAYER.HETERO;
-        return SEX_OF_PLAYER.UNI;
-    }
-
-    private static String getRandomName(SEX_OF_PLAYER sexOfPlayer) {
-        switch (sexOfPlayer) {
-            case HOMO:
-                return "Dusan";
-            case HETERO:
-                return "Kajka";
-            case UNI:
-            default:
-                return "Samko";
-        }
-    }
-
-    public static int getValue(PARAM_TYPE paramType, String line) {
-        String[] params = line.split(";");
-        if (params.length != 4) return 0;
-
-        try {
-            switch (paramType) {
-                case POINTS:
-                    return Integer.parseInt(params[1]);
-                case SIPS:
-                    return Integer.parseInt(params[2]);
-                case TIMER:
-                    return Integer.parseInt(params[3]);
-                default:
-                    return 0;
-            }
-        } catch (NumberFormatException e) {
-            return 0;
-        }
-
-    }*/
-
     private static Player getActivePlayer(){
         return Configuration.getPlayers()[KongosDrinkMainModel.getInstance().getPlayerIndex()];
     }
-
-    /*public static String replace(String text){
-        return text.split(";")[0];
-    }*/
 
     public static String replacePlayerNames(String text){
 
